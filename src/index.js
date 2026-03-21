32f4318117bdf4408c5fe449d8361cb8a9107327
Agustin Allende <agus.allende996@gmail.com>
2026-03-21 20:04:43 -0300

    fix servidor render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import app from "./app.js"
 
 
-app.listen(3000)
-console.log("Servidor iniciado 2...")
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Servidor corriendo en puerto ' + PORT);
+});
