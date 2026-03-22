5c4ecf1a8512001b06540a7f169d5c294086af4a
Agustin Allende <agus.allende996@gmail.com>
2026-03-22 18:50:40 -0300

    migrando categorias a postgres

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -2,16 +2,16 @@ import { getConnection } from '../database/connection.js'
 
 export const getCategorias = async (req, res) => {
     try {
+        console.log("ENTRA A CATEGORIAS")
+
         const pool = await getConnection()
 
-        const result = await pool.query(
-            "SELECT id_categoria, nombre FROM categoria"
-        )
+        const result = await pool.query("SELECT * FROM categoria")
 
         res.json(result.rows)
 
     } catch (error) {
-        console.error("ERROR EN CATEGORIAS:", error)
+        console.error("ERROR REAL:", error) // 👈 ESTO ES LA CLAVE
         res.status(500).json({ message: "Error al obtener categorias" })
     }
 }
